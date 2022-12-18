6beb04bdf50816c8a170c05136d433bf18433da6
Eemil Lehto <eemil.lehto@tuni.fi>
2022-12-18 12:17:00 +0200

    Added toNumber test

---
+++ test/toNumber.test.js
@@ -0,0 +1,10 @@
+var chai = require("chai")
+var isDate = require("../src/isNumber")
+const expect = chai.expect
+
+describe("toNumber", () => {
+    it("Converts value to number", () =>{
+        expect(toNumber("5")).to.equal(5)
+        expect(toNumber(Number.MIN_VALUE)).to.equal(5e-324)
+    });
+   })
