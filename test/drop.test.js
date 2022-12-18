45e0e3dd3acbe2f6a69d66868e2fca5447bf7d30
Eemil Lehto <eemil.lehto@tuni.fi>
2022-12-18 13:49:01 +0200

    Added fixes

--- test/drop.test.js
+++ test/drop.test.js
@@ -4,9 +4,9 @@ const expect = chai.expect
 
 describe("drop", () => {
     it("drop function drops one element", () =>{
-        expect(drop([1,4,5])).to.deep.qual([4,5])
+        expect(drop([1,4,5])).to.deep.equal([4,5])
     });
-    it("drop function drops four element", () =>{
+    it("drop function drops four elements", () =>{
         expect(drop([1,4,5,7,3,1,5,4], 4)).to.deep.equal([3,1,5,4])
     });
     it("drop function drops four elements from an array that is in object", () =>{
