e0959bd79400165ee0a0465f169b576d9edff263
Eemil Lehto <eemil.lehto@tuni.fi>
2022-12-18 13:47:51 +0200

    Added fixes

--- test/drop.test.js
+++ test/drop.test.js
@@ -2,17 +2,17 @@ var chai = require("chai")
 var drop = require("../src/drop")
 const expect = chai.expect
 
-describe("add", () => {
+describe("drop", () => {
     it("drop function drops one element", () =>{
-        expect(drop([1,4,5])).to.equal([4,5])
+        expect(drop([1,4,5])).to.deep.qual([4,5])
     });
     it("drop function drops four element", () =>{
-        expect(drop([1,4,5,7,3,1,5,4], 4)).to.equal([3,1,5,4])
+        expect(drop([1,4,5,7,3,1,5,4], 4)).to.deep.equal([3,1,5,4])
     });
     it("drop function drops four elements from an array that is in object", () =>{
-        expect(drop({a: [1,4,5,7,3,1,5,4]}, 4)).to.equal([3,1,5,4])
+        expect(drop({a: [1,4,5,7,3,1,5,4]}, 4)).to.deep.equal([3,1,5,4])
     });
     it("drop function drops all elements", () =>{
-        expect(drop([1,4,5,7], 4)).to.equal([])
+        expect(drop([1,4,5,7], 4)).to.deep.equal([])
     });
    })
